5db23120a3a50a8a4bc5631215c08bcfb7f1da24
Urvashi-iPhone <lathiyaurvashi36@gmail.com>
2017-03-21 11:16:54 +0530

    first commit

---
+++ sttableview/stTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  stTableViewCell.h
+//  sttableview
+//
+//  Created by Tecksky Techonologies on 3/21/17.
+//  Copyright © 2017 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface stTableViewCell : UITableViewCell
+
+@end
---
+++ sttableview/stTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  stTableViewCell.m
+//  sttableview
+//
+//  Created by Tecksky Techonologies on 3/21/17.
+//  Copyright © 2017 Tecksky Technologies. All rights reserved.
+//
+
+#import "stTableViewCell.h"
+
+@implementation stTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
